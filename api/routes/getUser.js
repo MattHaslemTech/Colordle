6b6f90ea1b3b7431fc03ac944c248346cebc1920
MattHaslem <matt@matthaslem.tech>
2022-07-16 13:19:23 -0400

    Add extra security to getUser and insertUser APIs

--- api/routes/getUser.js
+++ api/routes/getUser.js
@@ -5,6 +5,8 @@ var router = express.Router();
 
 /*
  * Returns whole row from a selected User based on their ID
+ *
+ * @param user : the userId
  */
 router.get("/", function(req, res, next) {
 
@@ -15,15 +17,23 @@ router.get("/", function(req, res, next) {
       port: process.env.DB_PORT,
       user: process.env.DB_USER,
       password: process.env.DB_PASS,
-      database: process.env.DB_DB
+      database: process.env.DB_DB,
+      multipleStatements: false
     })
+
+    // Make sure user only has letters and numbers in it
+    const userPattern = /^[A-Za-z0-9]+$/;
+    if(!userId.match(userPattern)){
+      return res.status(400).json({err: "Invalid user ID!"});
+    }
 
     connection.connect();
 
-    var query = "SELECT * FROM `users` WHERE sessionId='" + userId + "'";
+    var query = "SELECT * FROM `users` WHERE sessionId = ?";
+    var queryParams = [userId];
 
 
-    connection.query(query, (err, rows, fields) => {
+    connection.query(query, queryParams, (err, rows, fields) => {
       if (err) throw err
 
       res.send(rows[0]);
--- api/routes/insertUser.js
+++ api/routes/insertUser.js
@@ -5,25 +5,34 @@ var router = express.Router();
 
 /*
  * Insert User into DB
+ *
+ * @param user : the userId
  */
 router.get("/", function(req, res, next) {
 
     var userId = req.query.user;
+
+    // Make sure user only has letters and numbers in it
+    const userPattern = /^[A-Za-z0-9]+$/;
+    if(!userId.match(userPattern)){
+      return res.status(400).json({err: "Invalid user ID!"});
+    }
 
     const connection = mysql.createConnection({
       host: process.env.DB_HOST,
       port: process.env.DB_PORT,
       user: process.env.DB_USER,
       password: process.env.DB_PASS,
-      database: process.env.DB_DB
+      database: process.env.DB_DB,
+      multipleStatements: false
     })
 
     connection.connect();
 
-    var query = "INSERT INTO `users` (sessionId) VALUES ('" + userId + "')";
+    var query = "INSERT INTO `users` (sessionId) VALUES ( ? )";
+    var queryParams = [userId];
 
-
-    connection.query(query, (err, rows, fields) => {
+    connection.query(query, queryParams, (err, rows, fields) => {
       if (err) throw err
       console.log(rows);
 
